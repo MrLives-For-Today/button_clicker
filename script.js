58cd7f2f59b628be5c8f5320b78ea8a2f3bc5986
MrLives-For-Today <imcalzada03@gmail.com>
2021-07-26 20:54:04 -0500

    Buttons Clicked!

---
+++ script.js
@@ -0,0 +1,14 @@
+// Login button 
+function change(element) {
+    element.innerText = "Logout"
+}
+
+// Add definition button 
+function disappear(element) {
+    element.remove();
+}
+
+// Like button 
+function theAlert() {
+    alert("Ninja was liked!");
+}
